300fa36397c4f53ee056a8278d644405a39aae98
mason66xue <117794323+mason66xue@users.noreply.github.com>
2023-02-05 11:12:50 -0500

    fixing bug in typeDefs

--- server/shcema/typeDefs.js
+++ server/shcema/typeDefs.js
@@ -2,33 +2,33 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
 type User {
-    _id: ID!
-    username: String!
-    email: String!
+    _id: ID
+    username: String
+    email: String
     bookCount: Int
-    password: String!
+    password: String
     savedBooks: [Book]
 }
 
 type Book {
-    bookId: ID!
+    bookId: ID
     authors: [String]
-    description: String!
-    title: String!
-    image: String!
-    link: String!
+    description: String
+    title: String
+    image: String
+    link: String
 }
 type Auth {
-    token: ID!
+    token: ID
     user: User
 }
 input BookInput {
-    bookId: ID!
+    bookId: ID
     authors: [String]
-    description: String!
-    title: String!
-    image: String!
-    link: String!
+    description: String
+    title: String
+    image: String
+    link: String
     }
 
 
@@ -36,10 +36,10 @@ type Query {
     me: User
 }
 type Mutation{
-    login(email: String!, password: String!): Auth
-    addUser(username: String!, email: String!, password: String!): Auth
-    saveBook(bookData: BookInput!): User
-    removeBook(bookId: ID!): User
+    login(email: String, password: String): Auth
+    addUser(username: String, email: String, password: String): Auth
+    saveBook(bookData: BookInput): User
+    removeBook(bookId: ID): User
 
 }
 `;
